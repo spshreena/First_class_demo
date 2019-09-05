39d79de56970fac82fcba21812627d7022a10119
spshreena <54719859+spshreena@users.noreply.github.com>
2019-09-05 15:58:57 -0400

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,12 @@
+"""
+Calculae area and perimeter if a circle
+"""
+
+pi = 3.14
+r= 2.0
+area = pi*(r**2)
+perim = 2.0*pi*r
+
+print "given r: ", r
+print "Area is: ", area
+print "perimeter is:", perim
